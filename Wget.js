cd31281a424524687040ca9344fa1010f18e06bd
Yaroslav Dobzhanskij <merlyn.tgz@gmail.com>
2018-12-01 12:27:12 +0200

    v2.0.2 fixed wget downloader

--- Wget.js
+++ Wget.js
@@ -1,6 +1,7 @@
 const Download = require('./abstract/Download');
 const request = require('request');
 const progress = require('request-progress');
+const fs = require('fs');
 class Wget extends Download {
   go() {
     const { proxy, headers } = this.options || {};
@@ -15,6 +16,7 @@ class Wget extends Download {
       stream.on('progress', progress => this.updateProgress(progress.percent, 1, progress.size.transferred));
       stream.on('error', reject);
       stream.on('end', resolve);
+      stream.pipe(fs.createWriteStream(this.out));
     });
   }
 }
